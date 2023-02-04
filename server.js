18159853e8103d0da95bc5e2f016eaae40458847
20mca1382 <73593913+20mca1382@users.noreply.github.com>
2023-02-05 00:51:34 +0530

    Homepage is done

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const connectDB = require('./config/connectDB')
+dotenv.config();
+
+//database call
+connectDB()
+//rest object
+const app = express()
+
+//middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+//routes
+app.get('/', (req,res)=>{
+    res.send('<h1>Hello from server</h1>')
+})
+
+//PORT
+const PORT = 8080 || process.env.PORT
+
+//Listen server
+app.listen(PORT, ()=>{
+    console.log(`Server running on port : ${PORT}`)
+})
