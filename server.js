dcd5879805bde22456d2a7cc45a315ef15f31466
20mca1382 <73593913+20mca1382@users.noreply.github.com>
2023-02-09 17:57:42 +0530

    Completed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const dotenv = require('dotenv')
 const colors = require('colors')
 const connectDB = require('./config/connectDB')
+const path = require('path')
 dotenv.config();
 
 //database call
@@ -17,10 +18,19 @@ app.use(express.json())
 app.use(cors())
 
 //routes
-app.get('/', (req,res)=>{
-    res.send('<h1>Hello from server</h1>')
-})
+// app.get('/', (req,res)=>{
+//     res.send('<h1>Hello from server</h1>')
+// })
+app.use('/api/v1/users', require('./routes/userRoute'))
+//transection routes
+app.use('/api/v1/transections', require("./routes/transactionRoutes"))
 
+//static files
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
 //PORT
 const PORT = 8080 || process.env.PORT
 
